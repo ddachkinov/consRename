fbd3c482165b14bb4d3717c0b8e5f03e256e97dc
D.Dachkinov <ddachkinov@gmail.com>
2016-09-12 00:00:54 +0300

    innitial commit

---
+++ renameIt17.py
@@ -0,0 +1,19 @@
+import os
+
+def rename_files():
+    file_list = os.listdir(r"F:\DEV\Python\Secret Message\alphabet")
+   #print(file_list)
+    saved_path = os.getcwd()
+    os.chdir(r"F:\DEV\Python\Secret Message\alphabet")
+   # for i in range(0,1
+    file_list = saved_path[1:]
+    i = 1
+    for file_name in file_list:
+        
+        preimenuvai = os.path.splitext('.jpg')[i]
+        print("staro Ime - "+file_name)
+        print("Novo Ime - "+file_name+str(i))
+        os.rename(file_name,file_name+preimenuvai)
+        i = i + 1
+    os.chdir(saved_path)
+rename_files()
