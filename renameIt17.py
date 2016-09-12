03f7cbadabeeffb4cd8f4620b0979fa71489ca54
D.Dachkinov <ddachkinov@gmail.com>
2016-09-12 17:03:50 +0300

    complete change, now the file is able to rename everything, regardless the extension within the directory that has been executed

--- renameIt17.py
+++ renameIt17.py
@@ -1,16 +1,11 @@
 import os
 
 def rename_files():
-    file_list = os.listdir(r"F:\DEV\Python\Secret Message\alphabet")
-    saved_path = os.getcwd()
-    os.chdir(r"F:\DEV\Python\Secret Message\alphabet")
-    file_list = saved_path[1:]
-    i = 1
-    for file_name in file_list:  
-        preimenuvai = os.path.splitext('.jpg')[i]
-        print("staro Ime - "+file_name)
-        print("Novo Ime - "+file_name+str(i))
-        os.rename(file_name,file_name+preimenuvai)
-        i = i + 1
-    os.chdir(saved_path)
+    flist = os.listdir(os.getcwd())
+    flistC = flist[1:]
+    m = 0
+    for i in flistC:
+        fileext = os.path.splitext(i)[1]
+        os.rename(i,str(m)+fileext)
+        m = m+1
 rename_files()
